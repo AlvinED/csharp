815b5bb8bf35c81adec88685e84322efa10aa9cb
Donald J. Santos <DonaldJSantos@Gmail.com>
2014-12-02 20:58:20 -0500

    Final Exam Review
    Final Exam Review

---
+++ FinalExam/FinalExam/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FinalExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1;
+            r1 = new Rectangle();
+
+            r1.SetLength(10);
+            r1.SetWidth(20);
+
+            int W = r1.GetWidth();
+            int L = r1.GetLength();
+            int A = r1.ComputeArea();
+            int S = r1.ComputeScaledArea(3);
+        }
+    }
+}
---
+++ FinalExam/FinalExam/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿namespace FinalExam
+{
+    class Rectangle
+    {
+        private int _length;
+        private int _width;
+
+        public void SetLength(int len)
+        {
+            _length = len;
+        }
+
+        public void SetWidth(int wid)
+        {
+            _width = wid;
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+
+        public int GetLength()
+        {
+            return _length;
+        }
+
+        public int ComputeArea()
+        {
+            return _width * _length;
+        }
+
+        public int ComputeScaledArea(int scale)
+        {
+            return _width * _length * scale;
+        }
+    }
+}
