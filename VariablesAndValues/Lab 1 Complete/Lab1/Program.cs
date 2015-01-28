1459d023a9c768bf97b67dcfdc8572b177440692
Donald J. Santos <DonaldJSantos@Gmail.com>
2015-01-27 20:17:37 -0500

    Added Lab 1 Solution (Again)
    Added Lab 1 Solution (Again)

---
+++ VariablesAndValues/Lab 1 Complete/Lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            char middleInitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightInches;
+            double totalHeightCm;
+
+            firstName = "Donnie";
+            middleInitial = 'J';
+            lastName = "Santos";
+            fullName = firstName + " " + middleInitial + ". " + lastName;
+
+            age = 36;
+            heightFeet = 6;
+            heightInches = 1.25;
+            double totalInches = heightFeet * 12 + heightInches;
+            totalHeightCm = 2.54 * totalInches;
+
+            isCitizen = true;
+            bool isOldEnoughToVote = age >= 18;
+            canVote = isCitizen && isOldEnoughToVote;
+        }
+    }
+}
